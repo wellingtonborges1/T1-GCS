f0f3710405a376fc05a2a5736bd4d04ad0566290
Lorenzo Panato <lorenzohpanato@gmail.com>
2023-09-28 19:52:04 -0300

    corrigindo bug de merge

--- src/app/Sistema.java
+++ src/app/Sistema.java
@@ -1,9 +1,11 @@
 package app;
 
+import modelo.Custo;
 import modelo.Departamento;
 import modelo.Empresa;
 import modelo.Funcionario;
 
+import java.util.Date;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -79,8 +81,8 @@ public class Sistema {
 
     public void adicionarFuncionario(Funcionario funcionario){
         for(Funcionario f : empresa.getFuncionarios()){
-            if(Objects.equals(f.getMatricula(), funcionario.getMatricula()))
-                throw new IllegalArgumentException("Funcionário já existe na empresa");
+            if(f.getMatricula() == funcionario.getMatricula())
+                System.out.println("Funcionário já existe na empresa");
         }
         empresa.getFuncionarios().add(funcionario);
         funcionario.getDepartamento().getFuncionarios().add(funcionario);
@@ -124,6 +126,31 @@ public class Sistema {
         System.out.println("Funcionario logado com sucesso!");
         f.setLog(true);
         return true;
-        }
+    }
 
+    //MANIPULACAO DOS CUSTOS
+
+    public void adicionarCusto(Funcionario funcionario, Custo custo) {
+        if(funcionario.getLog())
+            funcionario.getDepartamento().getCustos().add(custo);
+        else
+            System.out.println("Funcionário não está logado");
+    }
+
+    public void removerCusto(Departamento d) {
+        Custo x = new Custo(0, null, new Date("01/01/50000"), null, d);
+        for (Custo custo : d.getCustos()) {
+            if(custo.getData().compareTo(x.getData()) <0) {
+                x=custo;
+            }
+        }
+        d.getCustos().remove(x);
+    }
+
+    public void todosCustos(Departamento d) {
+        for (Custo custo : d.getCustos()) {
+            System.out.println(custo.toString());
+        }
+    }
+}
 }
