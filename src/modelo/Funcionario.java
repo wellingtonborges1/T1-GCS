4a8edeff0caa411d717aa437d94003a62453cb46
salocod <nicolas.docolas1804@gmail.com>
2023-09-28 16:17:41 -0300

    sistema login

--- src/modelo/Funcionario.java
+++ src/modelo/Funcionario.java
@@ -5,11 +5,13 @@ public class Funcionario {
     private int matricula;
     private String nome;
     private Departamento departamento;
+    private boolean estaLogado;
 
     public Funcionario(Integer matricula, String nome, Departamento departamento) {
         this.matricula = matricula;
         this.nome = nome;
         this.departamento = departamento;
+        estaLogado = false;
     }
 
     public Integer getMatricula() {
@@ -22,6 +24,14 @@ public class Funcionario {
 
     public Departamento getDepartamento() {
         return departamento;
+    }
+
+    public boolean getLog() {
+        return estaLogado;
+    }
+
+    public boolean setLog(boolean valor) { 
+        return this.estaLogado = valor;
     }
 
     @Override
